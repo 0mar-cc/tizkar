5d3862b19669ad9e5179894629ad0563df551dfb
0mar-cc <omarmoh3010@gmail.com>
2024-05-18 05:20:53 +0300

    edited the best sellers names and prices and finched from the ategories templates and finished from the new prodcts slide

--- main.js
+++ main.js
@@ -86,3 +86,33 @@ prev.addEventListener("click", e => {
 
 let width = carousel.offsetWidth;
 window.addEventListener("resize", e => (width = carousel.offsetWidth));
+
+
+const gap2 = 16;
+
+const carousel2 = document.getElementById("carousel2"),
+  content2 = document.getElementById("content2"),
+  next2 = document.getElementById("next2"),
+  prev2 = document.getElementById("prev2");
+
+next2.addEventListener("click", s => {
+  carousel2.scrollBy(width2 + gap2, 0);
+  if (carousel2.scrollWidth !== 0) {
+    prev2.style.display = "flex";
+  }
+  if (content2.scrollWidth - width2 - gap2 <= carousel2.scrollLeft + width2) {
+    next2.style.display = "none";
+  }
+});
+prev2.addEventListener("click", s => {
+  carousel2.scrollBy(-(width2 + gap2), 0);
+  if (carousel2.scrollLeft - width2 - gap2 <= 0) {
+    prev2.style.display = "none";
+  }
+  if (!content2.scrollWidth - width2 - gap2 <= carousel2.scrollLeft + width2) {
+    next2.style.display = "flex";
+  }
+});
+
+let width2 = carousel2.offsetWidth;
+window.addEventListener("resize", s => (width2 = carousel2.offsetWidth));
